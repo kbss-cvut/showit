adbb2a3fc855e6ca3ae47d457657bfd57b979a8e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2026-08-05 14:16:07 +0200

    [GH-2] Show notations as chips next to term pref label.

--- src/components/terms/TermHeader.tsx
+++ src/components/terms/TermHeader.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { Box } from "@mui/material";
 import AltLabel from "../detail_common/AltLabel";
 import DetailPageHeader from "../detail_common/DetailPageHeader";
 import RouteLink from "../RouteLink";
 import { generateVocabularyRoute } from "../../utils/Utils";
 import { TermInterface } from "../../api/data/terms";
+import { Notations } from "./Notations";
 
 export interface DetailHeaderProps {
   term: TermInterface;
 }
 
 const TermHeader: React.FC<DetailHeaderProps> = ({ term }) => {
-  const { $id, label, altLabels, vocabulary } = term;
+  const { $id, label, altLabels, notation, vocabulary } = term;
 
   const vocabularyRoute = generateVocabularyRoute(vocabulary.$id);
   const above = (
@@ -20,10 +22,22 @@ const TermHeader: React.FC<DetailHeaderProps> = ({ term }) => {
   );
   const below = <AltLabel altLabels={altLabels} />;
 
+  const title =
+    notation.length > 0 ? (
+      <>
+        {label}
+        <Box component="span" sx={{ marginLeft: "0.25rem" }}>
+          <Notations notation={notation} />
+        </Box>
+      </>
+    ) : (
+      label
+    );
+
   return (
     <DetailPageHeader
       aboveLabel={above}
-      label={label}
+      label={title}
       belowLabel={below}
       iri={$id}
     />
