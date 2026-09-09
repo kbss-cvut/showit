c32ef5276eb08d39808160e9d325acddb12262b0
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2026-09-09 13:52:36 +0200

    [GH-9] Add a plugin for generating concatenated notation from multiple added terms.

---
+++ src/plugin/CasNotationConcatGeneratorPlugin.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Box, Button, Paper, Stack, Typography } from "@mui/material";
+import { Add as AddIcon, Cancel as CancelIcon } from "@mui/icons-material";
+import { TermInterface } from "../api/data/terms";
+import { PluginMetadata } from "./PluginApi";
+import { getLocalized } from "../utils/IntlUtils";
+import { useLanguage } from "../context/LanguageContext";
+
+const initialState = {
+  notation: "",
+  label: "",
+};
+let storedState = { ...initialState };
+
+const CasNotationConcatGeneratorPlugin: React.FC<{ data: TermInterface }> = ({
+  data,
+}) => {
+  const { language } = useLanguage();
+  const [notation, setNotation] = React.useState(storedState.notation);
+  const [label, setLabel] = React.useState(storedState.label);
+  const onAdd = () => {
+    const newLabel =
+      label +
+      (label.length > 0 ? " " : "") +
+      getLocalized(data.label, language);
+    const newNotation =
+      notation +
+      (data.notation.length > 0 ? data.notation.join() : data.notation);
+    setLabel(newLabel);
+    setNotation(newNotation);
+    storedState = {
+      label: newLabel,
+      notation: newNotation,
+    };
+  };
+  const onReset = () => {
+    setNotation("");
+    setLabel("");
+    storedState = { ...initialState };
+  };
+
+  return (
+    <Paper
+      elevation={4}
+      sx={{
+        minWidth: "33%",
+        position: "fixed",
+        bottom: 16,
+        left: "50%",
+        transform: "translateX(-50%)",
+        px: 3,
+        py: 1.5,
+        display: "flex",
+        alignItems: "center",
+        gap: 2,
+        zIndex: (theme) => theme.zIndex.tooltip,
+      }}
+    >
+      <Stack direction="column" spacing={0.5} width="100%">
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ justifyContent: "space-between" }}
+        >
+          <Typography variant="h6">Generátor notačních kódů</Typography>
+          <Box>
+            <Button onClick={onAdd} color="primary">
+              <AddIcon />
+            </Button>
+            <Button onClick={onReset} color="secondary">
+              <CancelIcon />
+            </Button>
+          </Box>
+        </Stack>
+        <Typography variant="body1">
+          <Typography component="span" fontWeight="fontWeightBold">
+            Název:
+          </Typography>{" "}
+          {label}
+        </Typography>
+        <Typography variant="body1">
+          <Typography component="span" fontWeight="fontWeightBold">
+            Notace:
+          </Typography>{" "}
+          {notation}
+        </Typography>
+      </Stack>
+    </Paper>
+  );
+};
+
+export const metadata: PluginMetadata = {
+  id: "CasNotationConcatGeneratorPlugin",
+  name: "CasNotationConcatGeneratorPlugin",
+  view: "TermDetail",
+};
+
+export default CasNotationConcatGeneratorPlugin;
