d875c2def643488b8afde89159a1fe2844307898
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2026-09-09 12:38:28 +0200

    [GH-9] Implement a plugin system for the app.

--- src/components/vocabularies/VocabulariesPage.tsx
+++ src/components/vocabularies/VocabulariesPage.tsx
@@ -5,20 +5,27 @@ import ErrorPage from "../ErrorPage";
 import NoResults from "../search/NoResults";
 import VocabularyAndTermsListWindow from "./VocabularyAndTermsListWindow";
 import { generateVocabularyRoute } from "../../utils/Utils";
+import { useVocabularyListPlugins } from "../../plugin/PluginRegistry";
 
 const VocabulariesPage: React.FC = () => {
   const { data, isLoading, isSuccess, isError } = useAllVocabularies();
+  const plugins = useVocabularyListPlugins();
   if (isLoading) return <Loader />;
   if (isError || !data) return <ErrorPage />;
 
   if (isSuccess) {
     return (
-      <VocabularyAndTermsListWindow
-        data={data}
-        routeResolver={generateVocabularyRoute}
-        listLabel={"Slovníky"}
-        searchHelperText={"Zadejte hledaný slovník"}
-      />
+      <>
+        <VocabularyAndTermsListWindow
+          data={data}
+          routeResolver={generateVocabularyRoute}
+          listLabel={"Slovníky"}
+          searchHelperText={"Zadejte hledaný slovník"}
+        />
+        {plugins.map(({ id, Component }) => (
+          <Component key={id} data={data} />
+        ))}
+      </>
     );
   }
   return <NoResults />;
